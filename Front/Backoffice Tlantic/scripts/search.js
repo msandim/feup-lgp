02d624f644cf8d645a2b7f2854d52142fe64abbb
NunoGhost <up201502854@fe.up.pt>
2016-04-21 16:04:04 +0100

    Changes to Filter and Expand
    Filter is no longer case sensitive to allow for easier search.
    Expand now changes the button arrow icon when pressed

--- Front/Backoffice Tlantic/scripts/search.js
+++ Front/Backoffice Tlantic/scripts/search.js
@@ -2,16 +2,21 @@ function filter(){
 
   var ViewModel = function() {
     var self = this;
-      
+
       self.filter = ko.observable('');
-      
+
       self.items = ko.observableArray(["Televisão", "Computador", "Micro-ondas", "Frigorífico", "Telemóvel", "Colunas", "Rádio",
-        "Televisão", "Computador", "Frigorífico", "Telemóvel", "Colunas", "Rádio", "Televisão", "Computador"]);
-      
+        "Televisão", "Computador", "Frigorífico", "Telemóvel", "Colunas", "Rádio", "Televisão", "Computador", "Micro-ondas", "Frigorífico",
+         "Telemóvel", "Colunas", "Rádio", "Televisão", "Computador", "Frigorífico", "Telemóvel", "Colunas", "Rádio"]);
+
+      self.items.sort();
+
       self.filteredItems = ko.computed(function() {
-        var filter = self.filter();
-        if (!filter) { return self.items(); }
-        return self.items().filter(function(i) { return i.indexOf(filter) > -1; });
+        var filter = self.filter().toLowerCase();
+        if(!filter) { return self.items(); }
+        return self.items().filter(function(i) {
+          return i.toLowerCase().indexOf(filter) > -1;
+        });
       });
     };
 
@@ -20,7 +25,14 @@ function filter(){
 }
 
 function expand() {
+  var self = this;
   $('#expand-one').click(function(){
       $('.content-one').slideToggle('slow');
+      var spanClass = $("#expand-one span").attr("class");
+      if(spanClass == "glyphicon glyphicon-menu-up"){
+        $("#expand-one span").attr("class","glyphicon glyphicon-menu-down");
+      }else{
+        $("#expand-one span").attr("class","glyphicon glyphicon-menu-up");
+      }
   });
 }
