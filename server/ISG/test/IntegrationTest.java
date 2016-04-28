bb5e4c59a80a39983a363d95247d63485fd245fb
Sapphirus4 <up201106586@fe.up.pt>
2016-04-28 10:41:04 +0100

    Tests related to creating and removing categories are almost done.

--- server/ISG/test/IntegrationTest.java
+++ server/ISG/test/IntegrationTest.java
@@ -53,18 +53,63 @@ public class IntegrationTest extends WithServer {
     @Test
     public void testCreateCategory() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readValue(new File("addc/Request1.json"), JsonNode.class);
 
-        WSResponse response = postRequest("api/addc", rootNode);
-
-        assert response != null;
-        assertEquals("{}", response.getBody()); //TODO: Response should be {}
-
-        response = getRequest("category/0");
+        WSResponse response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
 
         assert response != null;
-        assertEquals("Category{id=0, name=Televisoes} \n", response.getBody()); //TODO change to JSON
-        //assertEquals(rootNode, response.asJson());
+        assertEquals(mapper.readValue(new File("addc/Response1.json"), JsonNode.class), response.asJson());
+
+        //response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
+        response = getRequest("category/0"); //TODO how to check if category was added? This route is not on the Architecture Report / Acceptance Test Plan.
+
+        assert response != null;
+        assertEquals("Category{id=0, name=Televisoes} \n", response.getBody()); //TODO Change to JSON / Change to a proper response
+        //assertEquals(mapper.readValue(new File("addc/Response2.json"), JsonNode.class), response.asJson());
+    }
+
+    @Test
+    public void testCreateCategoryError() throws Exception { //TODO Can be compressed in 1 test for normal insert and error insert
+        ObjectMapper mapper = new ObjectMapper();
+
+        WSResponse response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("addc/Response1.json"), JsonNode.class), response.asJson());
+
+        response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("addc/Response2.json"), JsonNode.class), response.asJson());
+    }
+
+    @Test
+    public void testRemoveCategory() throws Exception { //TODO verify what happens when the category has questions or products
+        ObjectMapper mapper = new ObjectMapper();
+
+        WSResponse response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("addc/Response1.json"), JsonNode.class), response.asJson());
+
+        response = postRequest("api/removec", mapper.readValue(new File("removec/Request1.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("remvec/Response1.json"), JsonNode.class), response.asJson());
+    }
+
+    @Test
+    public void testRemoveCategoryError() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        WSResponse response = postRequest("api/addc", mapper.readValue(new File("addc/Request1.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("addc/Response1.json"), JsonNode.class), response.asJson());
+
+        response = postRequest("api/removec", mapper.readValue(new File("removec/Request2.json"), JsonNode.class));
+
+        assert response != null;
+        assertEquals(mapper.readValue(new File("remvec/Response2.json"), JsonNode.class), response.asJson());
     }
 
     @Test
