2a89aaa5e05946ebefbbc4f32e75a055491ce983
lycantropus <mechanical.maggot.0@gmail.com>
2016-04-30 10:28:00 +0100

    new crypto key

--- server/ISG/deploy-scripts/deploy.sh
+++ server/ISG/deploy-scripts/deploy.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
+cd /feup-lgp/server/ISG
 
-cd ~/feup-lgp/server/ISG
+# kill previous running app (if any)
+kill $(cat target/universal/ISG-1.0-SNAPSHOT/RUNNING_PID)
 
-ls
+git pull
 
-kill $(cat target/universal/isg-1.0-SNAPSHOT/RUNNING_PID)
-
-git pull origin suicidal_deployment
-
-activator dist
+./activator dist
 
 cd target/universal/
 rm -r isg-1.0-SNAPSHOT
 unzip isg-1.0-SNAPSHOT.zip
 cd isg-1.0-SNAPSHOT/bin/
 
-ISG -Dplay.evolutions.db.default.autoApply=true -Dplay.crypto.secret="6dfq7vn`ctWLh?Qr8Y9LrvqZI4ae_mdBtI4iHa^XrV?R1TfIRq0lBExsbxsoVnGx"
+./isg -Dplay.evolutions.db.default.autoApply=true -Dplay.crypto.secret="Hcey3kRQ@?=_S__2H7pvQ@mkxJ8kF`KAe7X<<K3:56@X^B>m/Owzevh<C@D`be/J" &
