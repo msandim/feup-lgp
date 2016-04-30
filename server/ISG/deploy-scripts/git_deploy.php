92f194d2f1e1df4cca9f5210069eaac9cce856fd
lycantropus <mechanical.maggot.0@gmail.com>
2016-04-30 11:33:43 +0100

    hook time

--- server/ISG/deploy-scripts/git_deploy.php
+++ server/ISG/deploy-scripts/git_deploy.php
@@ -6,7 +6,7 @@ try {
 }
 // log the request
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
-if ($payload->ref === 'refs/heads/suicidal_branch') {
+if ($payload->ref === 'refs/heads/suicidal_deployment') {
 	echo 'deploying from github', PHP_EOL;
 	$message = shell_exec("/var/www/html/deploy_wrapper 2>&1");
 	print_r($message);
