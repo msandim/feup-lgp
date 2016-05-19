38a067c33609825a1132b51dd67ca1b8abbd0f6d
NunoGhost <up201502854@fe.up.pt>
2016-05-20 00:19:43 +0100

    Services working in browser (must use  chrome CORS plug-in)

--- Front/PhoneGap/www/js/one_page.js
+++ Front/PhoneGap/www/js/one_page.js
@@ -1,59 +1,26 @@
 //GLOBALS
 
+//Servidor e serviços
+var configs={
+	server:"http://104.167.113.111:9000",
+	getNextQuestionService:"/api/getNextQuestion",
+	sendFeedbackService:"/api/sendFeedback",
+  getAllCategoriesService:"/api/allCategories"
+}
+
 //Trocar por serviço
-var categoryArray =[
-  {
-    'name': "Televisão",
-    'image': "img/tv1.jpg"
-  },
-  {
-    'name': "Televisão",
-    'image': "img/tv1.jpg"
-  },
-  {
-    'name': "Televisão",
-    'image': "img/tv1.jpg"
-  },
-  {
-    'name': "Not TV",
-    'image': "img/tv2.jpg"
-  },
-  {
-    'name': "Not TV",
-    'image': "img/tv2.jpg"
-  },
-  {
-    'name': "Not TV",
-    'image': "img/tv2.jpg"
-  },
-  {
-    'name': "Televisão",
-    'image': "img/tv1.jpg"
-  }
-];
+var categoryArray;
 
 var toNextPage = {
   'category':"",
-  'question-answer':[],
+	'code':"",
+  'answers':[],
   'blacklist_questions':[],
-  'products':[
-    {
-      "id":"2",
-      "product_code":"XPTO-1234",
-      "name":"Televisão XPTO",
-      "score":0.9
-    },
-    {
-      "id":"17",
-      "product_code":"XPTO-1224",
-      "name":"Televisão XPTO_2",
-      "score":0.7
-    }
-  ],
+  'products':[],
   'history':[]
 };
 
-var service = {
+var service/* = {
   "question":{
     "id":"1",
     "text":"Qual o tamanho da sua sala?"
@@ -73,24 +40,58 @@ var service = {
     }
   ],
   "products":[]
-};
+}*/;
 
 function autoFillIndexPage(){
   var self=this;
-  //var serviceCall //get categoryArray
-  $('.main').empty();
-  $('.main').append('<h1 class="h1-title">Escolha uma categoria</h1><div class="interface-square interface-size"></div>');
-  categoryArray.forEach(function(category){
-    var interfaceSquare = $('.interface-square');
-    interfaceSquare.append('<div class="col-sm-4"><div class="thumbnail thumbnail-pad"><button type="button" class="btn btn-block button-pad autoFillFirstQuestionPage"><img src="'+category['image']+'" width ="200"><p class="text-center">'+category['name']+'</p></div></div>');
-  });
+  var serviceCall;
+	var stringified;
 
-  $('.autoFillFirstQuestionPage').click(function(){
-    toNextPage['category'] = $(this).text();
-    //var serviceCall pedir nova pergunta
-    autoFillFirstQuestionPage();
-  });
+	$('.main').empty();
+	$('.main').append('<h1 class="h1-title">Escolha uma categoria</h1><div class="interface-square interface-size"></div>');
 
+	//get categoryArray
+  $.ajax({
+        url: configs.server+configs.getAllCategoriesService,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        type: "GET",
+        crossDomain: true,
+			}).done(function(response){
+				categoryArray= response;
+				categoryArray.forEach(function(category){
+					var interfaceSquare = $('.interface-square');
+					interfaceSquare.append('<div class="col-sm-4"><div class="thumbnail thumbnail-pad"><button type="button" class="btn btn-block button-pad autoFillFirstQuestionPage" id="'+category['code']+'"><img src="'/*+category['image']*/+'" width ="200"><p class="text-center">'+category['name']+'</p></div></div>');
+				});
+
+				$('.autoFillFirstQuestionPage').click(function(){
+					toNextPage['category'] = $(this).text();
+					toNextPage['code']=$(this).attr("id");
+
+					serviceCall = {
+													 "category": toNextPage['code'],
+													 "answers": [ ],
+													 "blacklist_questions": [ ]
+												 };
+					//pedir nova pergunta
+					stringified = JSON.stringify(serviceCall);
+					$.ajax({
+								url: configs.server+configs.getNextQuestionService,
+								contentType: "application/json; charset=utf-8",
+								dataType: "json",
+								data: stringified,
+								type: "POST",
+								crossDomain: true,
+					}).done(function(response){
+								service = response;
+								autoFillFirstQuestionPage();
+					}).fail(function(err){
+								alert(err);
+					});
+				});
+			}).fail(function(err){
+				alert(err);
+			});
 };
 
 function autoFillFirstQuestionPage(){
@@ -109,18 +110,18 @@ function autoFillFirstQuestionPage(){
     var answerChosenID;
     service['answers'].forEach(function(answer){
       if(answer['text'] == answerChosenText){
-        answerChosenID = answer['id'];
+        answerChosenID = answer['code'];
       }
     });
 
-    toNextPage['question-answer'].push(
+    toNextPage['answers'].push(
         {
           'question':{
-            "id":service['question']['id'],
+            "code":service['question']['code'],
             "text":service['question']['text']
           },
           'answer':{
-            "id":answerChosenID,
+            "code":answerChosenID,
             "text":answerChosenText
           },
         }
@@ -135,8 +136,36 @@ function autoFillFirstQuestionPage(){
           },
         }
     );
-    //var serviceCall atualizar produtos
-    autoFillHubPage();
+    //var serviceCall atualizar produtos e pede pergunta
+		var serviceCall = {
+    	"category": toNextPage['code'],
+    	"answers": [],
+    	"blacklist_questions": [ ]
+		};
+		toNextPage['answers'].forEach(function(combo){
+			serviceCall['answers'].push({
+				"question":combo['question']['code'],
+				"answer":combo['answer']['code']
+			});
+		});
+		toNextPage['blacklist_questions'].forEach(function(question){
+			serviceCall['blacklist_questions'].push(question);
+		});
+		console.log(serviceCall);
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					service = response;
+					autoFillHubPage();
+		}).fail(function(err){
+					alert(err);
+		});
   });
 
   $('.backStep').click(function(){
@@ -145,7 +174,7 @@ function autoFillFirstQuestionPage(){
 
   $('.skipQuestion').click(function(){
     var answerChosenText = $(this).text();
-    toNextPage['blacklist_questions'].push(service['question']['id']);
+    toNextPage['blacklist_questions'].push(service['question']['code']);
     toNextPage['history'].push(
         {
           'question':{
@@ -156,23 +185,61 @@ function autoFillFirstQuestionPage(){
           },
         }
     );
-    autoFillHubPage();
+		//var serviceCall atualizar produtos e pede pergunta
+		var serviceCall = {
+    	"category": toNextPage['code'],
+    	"answers": [],
+    	"blacklist_questions": [ ]
+		};
+		toNextPage['answers'].forEach(function(combo){
+			serviceCall['answers'].push({
+				"question":combo['question']['code'],
+				"answer":combo['answer']['code']
+			});
+		});
+		toNextPage['blacklist_questions'].forEach(function(question){
+			serviceCall['blacklist_questions'].push(question);
+		});
+		console.log(serviceCall);
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					service = response;
+					console.log(service);
+					autoFillHubPage();
+		}).fail(function(err){
+					alert(err);
+		});
   });
 };
 
 function autoFillHubPage(){
   $('.main').empty();
   $('.main').append('<h4 class="h4-above-title" style="margin-left:0.5%;">Categoria: '+toNextPage['category']+'</h4><div class="row" style="margin-left:0.5%;"><h1 class="h1-title col-sm-8">Lista atual de produtos</h1><h1 class="h1-title col-sm-4" style="margin-left:-1%;">Histórico</h1></div><div class="row" style="margin-right:0.5%; margin-left:0.5%;"><div class="col-sm-7 interface-square interface-size product-table" style="padding-left: 20px;"></div><div class="col-sm-1"></div><div class="interface-square col-sm-4 interface-size history"></div></div><div class="buttons-row"><div class="row"><div class= "col-ms-2"><button type="button" class="btn btn-default pull-right autoFillQuestionPage">Próxima pergunta</button><button type="button" class="btn btn-default pull-right autoFillFeedbackPage" style="margin-right:10px;">Finalizar</button></div></div></div>');
-  toNextPage['products'].forEach(function(product){
-    $('.product-table').append('<div class="row"> <div class="col-sm-2"><div class="thumbnail"><img src="img/tv1.jpg" height ="50"></div></div><div class="col-sm-6"><p class="center-div">'+product['name']+'</p></div><div class="col-sm-2"><p class="center-div">1399,00<span class="glyphicon glyphicon-eur"></span></p></div><div class="col-sm-2 text-right"><p class="center-div-off">'+product['score']+' <span class="glyphicon glyphicon-arrow-up"></span></p><p>Soma: 49</p></div></div>');
+  service['products'].forEach(function(product){
+		addProductAndCalculateScoreDifference(product);
+	});
+	scoreSort();
+	toNextPage['products'].forEach(function(product){
+    $('.product-table').append('<div class="row"> <div class="col-sm-2"><div class="thumbnail"><img src="img/tv1.jpg" height ="50"></div></div><div class="col-sm-6"><p class="center-div">'+product['name']+'</p></div><div class="col-sm-2"><p class="center-div">1399,00<span class="glyphicon glyphicon-eur"></span></p></div><div class="col-sm-2 text-right"><p class="center-div-off">'+product['scoreChange']+' <span class="glyphicon glyphicon-arrow-up"></span></p><p>Score:'+product['score']+'</p></div></div>');
   });
   toNextPage['history'].forEach(function(combo){
     $('.history').append('<div><p>P.: '+combo['question']['text']+'</p><p>R.: '+combo['answer']['text']+'</p></div>');
   });
 
   $('.autoFillQuestionPage').click(function(){
-    //var serviceCall pedir nova pergunta
-    autoFillQuestionPage();
+		if(service['answers'].length==0){
+			alert("Não existem mais perguntas para esta categoria. Pressione OK para redirecionar para página final");
+			autoFillFeedbackPage();
+		}else{
+			autoFillQuestionPage();
+		}
   });
 
   $('.autoFillFeedbackPage').click(function(){
@@ -196,18 +263,18 @@ function autoFillQuestionPage(){
     var answerChosenID;
     service['answers'].forEach(function(answer){
       if(answer['text'] == answerChosenText){
-        answerChosenID = answer['id'];
+        answerChosenID = answer['code'];
       }
     });
 
-    toNextPage['question-answer'].push(
+    toNextPage['answers'].push(
         {
           'question':{
-            "id":service['question']['id'],
+            "code":service['question']['code'],
             "text":service['question']['text']
           },
           'answer':{
-            "id":answerChosenID,
+            "code":answerChosenID,
             "text":answerChosenText
           },
         }
@@ -222,8 +289,37 @@ function autoFillQuestionPage(){
           },
         }
     );
-    //var serviceCall atualizar produtos
-    autoFillHubPage();
+		//var serviceCall atualizar produtos e pede pergunta
+		var serviceCall = {
+    	"category": toNextPage['code'],
+    	"answers": [],
+    	"blacklist_questions": [ ]
+		};
+		toNextPage['answers'].forEach(function(combo){
+			serviceCall['answers'].push({
+				"question":combo['question']['code'],
+				"answer":combo['answer']['code']
+			});
+		});
+		toNextPage['blacklist_questions'].forEach(function(question){
+			serviceCall['blacklist_questions'].push(question);
+		});
+		console.log(serviceCall);
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					service = response;
+					autoFillHubPage();
+		}).fail(function(err){
+					alert(err);
+		});
+    //autoFillHubPage();
   });
 
   $('.autoFillFeedbackPage').click(function(){
@@ -232,7 +328,7 @@ function autoFillQuestionPage(){
 
   $('.skipQuestion').click(function(){
     var answerChosenText = $(this).text();
-    toNextPage['blacklist_questions'].push(service['question']['id']);
+    toNextPage['blacklist_questions'].push(service['question']['code']);
     toNextPage['history'].push(
         {
           'question':{
@@ -243,35 +339,126 @@ function autoFillQuestionPage(){
           },
         }
     );
-    autoFillHubPage();
+		//var serviceCall atualizar produtos e pede pergunta
+		var serviceCall = {
+    	"category": toNextPage['code'],
+    	"answers": [],
+    	"blacklist_questions": [ ]
+		};
+		toNextPage['answers'].forEach(function(combo){
+			serviceCall['answers'].push({
+				"question":combo['question']['code'],
+				"answer":combo['answer']['code']
+			});
+		});
+		toNextPage['blacklist_questions'].forEach(function(question){
+			serviceCall['blacklist_questions'].push(question);
+		});
+		console.log(serviceCall);
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					service = response;
+					autoFillHubPage();
+		}).fail(function(err){
+					alert(err);
+		});
   });
 
 };
 
 function autoFillFeedbackPage(){
   $('.main').empty();
+	var serviceCall = {
+		"category": toNextPage['code'],
+		"answers": [],
+		"feedback":-1
+	};
+	toNextPage['answers'].forEach(function(combo){
+		serviceCall['answers'].push({
+			"question":combo['question']['code'],
+			"answer":combo['answer']['code']
+		});
+	});
   $('.main').append('<h4 class="h4-above-title" style="margin-left:0.5%;">Categoria: '+toNextPage['category']+'</h4><h1 class="h1-title">Lista final de produtos</h1><div class="interface-square interface-size"></div><div class="buttons-row"><div class="row"><div class= "col-ms-2"><h4 class="pull-left">Esta sequência foi útil?</h4><button type="button" class="btn btn-default col-sm-2 pull-right no-button">Não</button><button type="button" class="btn btn-default col-sm-2 pull-right yes-button" style="margin-right:10px;">Sim</button></div></div></div>');
-  toNextPage['products'].forEach(function(product){
-    $('.interface-square').append('<div class="row"> <div class="col-sm-2"><div class="thumbnail"><img src="img/tv1.jpg" height ="50"></div></div><div class="col-sm-6"><p class="center-div">'+product['name']+'</p></div><div class="col-sm-2"><p class="center-div">1399,00<span class="glyphicon glyphicon-eur"></span></p></div><div class="col-sm-2 text-right"><p class="center-div">Soma: 49</p></div></div>');
+  service['products'].forEach(function(product){
+    $('.interface-square').append('<div class="row"> <div class="col-sm-2"><div class="thumbnail"><img src="img/tv1.jpg" height ="50"></div></div><div class="col-sm-6"><p class="center-div">'+product['name']+'</p></div><div class="col-sm-2"><p class="center-div">1399,00<span class="glyphicon glyphicon-eur"></span></p></div><div class="col-sm-2 text-right"><p class="center-div">Score:'+product['score']+'</p></div></div>');
   });
 
+7
+
   $('.yes-button').click(function(){
-    //var serviceCall envia valor
-    finishAndRestart();
+    serviceCall['feedback']=1;
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					finishAndRestart();
+		}).fail(function(err){
+					alert(err);
+		});
   });
 
   $('.no-button').click(function(){
-    //var serviceCall envia valor
-    finishAndRestart();
+    serviceCall['feedback']=0;
+		stringified = JSON.stringify(serviceCall);
+		$.ajax({
+					url: configs.server+configs.getNextQuestionService,
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					data: stringified,
+					type: "POST",
+					crossDomain: true,
+		}).done(function(response){
+					finishAndRestart();
+		}).fail(function(err){
+					alert(err);
+		});
+
   });
 
 };
 
 function finishAndRestart(){
   toNextPage['category'] = "";
-  toNextPage['question-answer']=[];
+	toNextPage['code']="";
+  toNextPage['answers']=[];
   toNextPage['blacklist_questions']=[];
-  //toNextPage['products']=[]; descomentar quando serviço estiver a funcionar
+  toNextPage['products']=[];
   toNextPage['history']=[];
   autoFillIndexPage();
+}
+
+function addProductAndCalculateScoreDifference(product){
+	var exists=false;
+		for(var i=0; i<toNextPage['products'].length; i++){
+			if(toNextPage['products'][i]['EAN'] == product['EAN']){
+				toNextPage['products'][i]['scoreChange'] = Math.round( (product['score']-toNextPage['products'][i]['score']) * 10 ) / 10;
+				toNextPage['products'][i]['score'] = Math.round( (product['score']) * 10 ) / 10;
+				exists=true;
+			}
+		}
+
+	if(!exists){
+		toNextPage['products'].push(product);
+		toNextPage['products'][toNextPage['products'].length-1]['scoreChange'] = Math.round( (product['score']) * 10 ) / 10;
+	}
+}
+
+function scoreSort(){
+	var byScore = toNextPage['products'];
+	byScore.sort(function(a,b) {
+		return b.score - a.score;
+	});
 }
